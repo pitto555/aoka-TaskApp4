bea02dd2de10d6cdcc859bd352f2b8540fb36785
aoka <aoka.555.ibiza@gmail.com>
2021-03-25 18:01:55 +0900

    First Commit

---
+++ TaskApp4/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  TaskApp4
+//
+//  Created by 光気増井 on 2021/03/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet private weak var label: UILabel!
+    
+    private var count = 0
+
+    @IBAction func plusButton(_ sender: Any) {
+        
+        count = count + 1
+        label.text =  String(count)
+ 
+    }
+    
+    @IBAction func cleaButton(_ sender: Any) {
+      
+        count = 0
+        label.text = String(count)
+        
+    }
+}
+
